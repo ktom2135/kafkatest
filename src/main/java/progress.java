9d65ad8c521112e51ce19b6cfc592d6be1c0acd1
xuchen <xuchen@thoughtworks.com>
2019-01-04 08:42:41 +0800

    again, add another project

--- src/main/java/progress.java
+++
@@ -1,8 +0,0 @@
-public class Progress {
-    public static void main(String[] args){
-        System.out.println("Hello world");
-
-        TestKafkaProducer testKafkaProducer  = new TestKafkaProducer();
-        testKafkaProducer.SendRecord();
-    }
-}
